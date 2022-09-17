a63c36ab445cd493928f362f8df0ce09c7d34bf2
Rômulo Melo <dev.eu.romulo.melo@gmail.com>
2022-09-17 11:52:37 -0300

    style: add color palette

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,25 @@
 /** @type {import('tailwindcss').Config} */
+// eslint-disable-next-line no-undef
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      gray: {
+        1: "#121214",
+        2: "#202024",
+        3: "#323238",
+        4: "#7C7C8A",
+        5: "#8D8D99",
+        6: "#C4C4CC",
+        7: "#E1E1E6"
+      },
+      green: {
+        DEFAULT: "#00875F",
+        light: "#00B37E"
+      },
+      red: "#F75A68",
+      white: "#FFFFFF"
+    },
     extend: {}
   },
   plugins: []
