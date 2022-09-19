344855f1171cb450e0bdd2d5b8e7bde8d659b6ee
Rômulo Melo <dev.eu.romulo.melo@gmail.com>
2022-09-19 15:20:44 -0300

    feat: add Comment component

--- src/components/Feed/index.tsx
+++ src/components/Feed/index.tsx
@@ -1,9 +1,9 @@
+import Comment from "../Comment"
+
 function Feed() {
   return (
     <main className="flex flex-1 flex-col gap-8">
-      {Array.from({ length: 8 }).map((_, index) => {
-        return <div key={index} className="h-[37.5rem] rounded-md bg-gray-2" />
-      })}
+      <Comment />
     </main>
   )
 }
