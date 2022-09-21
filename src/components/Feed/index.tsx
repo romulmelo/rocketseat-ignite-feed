f46fb1ddc4f329d37ad577c3dfff00d8a0a94819
Rômulo Melo <dev.eu.romulo.melo@gmail.com>
2022-09-21 12:53:14 -0300

    feat: get posts using fetch API

--- src/components/Feed/index.tsx
+++ src/components/Feed/index.tsx
@@ -1,9 +1,37 @@
+import { useEffect, useState } from "react"
 import Post from "../Post"
 
+interface Post {
+  id: number
+  author: {
+    avatarURL: string
+    name: string
+    role: string
+  }
+  content: [{ type: string; value: string }]
+  tags: string[]
+  publishedAt: Date
+}
+
 function Feed() {
+  const [posts, setPosts] = useState<Post[]>([])
+
+  useEffect(() => {
+    async function fetchPosts() {
+      const response = await fetch("http://localhost:3000/posts")
+      const data = await response.json()
+
+      setPosts(data)
+    }
+
+    fetchPosts()
+  }, [])
+
   return (
     <main className="flex flex-1 flex-col gap-8">
-      <Post />
+      {posts.map((post) => {
+        return <Post key={post.id} {...post} />
+      })}
     </main>
   )
 }
